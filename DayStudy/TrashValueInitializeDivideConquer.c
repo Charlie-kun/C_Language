508b24b197e39a6a27aa6eab5ab00ca8e2a5f11c
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-04-12 10:38:02 +0900

    Trash Value and initialize, Divide and conquer

---
+++ DayStudy/TrashValueInitializeDivideConquer.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  /*
+  //Trash Value and initialize
+
+  int a;
+  int b=20;    //initialize
+  int c;
+
+  a=10;    //initialize
+  printf("a : %d\n", a);    //10
+  printf("b : %d\n", b);    //20
+  printf("c : %d\n", c);    //trash value
+  */
+
+/*
+  //Divide and conquer
+  int a=3;
+
+  if(a<=2)
+  {
+    if(a==1)
+    {
+      printf("one");
+    }
+    else if(a==2)
+    {
+      printf("two");
+    }
+
+  }
+  else
+  {
+    if(a==3)
+    {
+      printf("three");
+    }
+    else if(a==4)
+    {
+      printf("four");
+    }
+  }
+  */
+  return 0;
+}
