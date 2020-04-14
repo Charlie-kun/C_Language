ae9382d0465a7668326b294e881541f9644d5dc8
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-04-14 17:51:20 +0900

    Add files via upload

---
+++ DayStudy/strtok.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUF_SIZE 100
+#define ARR_CNT 5
+
+int main(int argc, char *argv[])
+{
+	char *pToken;
+	char *pArray[ARR_CNT]={0}; 
+	int i=0;
+	char arge[BUF_SIZE];
+
+	if(argc!=2)
+	{
+		printf("Usage : %s SENSOR@<temputure>@<moisture>@<brightness>\n", argv[0]);
+		exit(1);
+	}
+		strcpy(arge,argv[1]);
+
+   		pToken = strtok(arge,"[@]");
+		while(pToken != NULL)
+		{
+			pArray[i] =  pToken;
+			if(i++ >= ARR_CNT)
+				break;
+			pToken = strtok(NULL,"[@]");
+		}
+  
+		printf("온도:%s, 습도:%s, 조도:%s\n",pArray[1],pArray[2],pArray[3]);
+}
