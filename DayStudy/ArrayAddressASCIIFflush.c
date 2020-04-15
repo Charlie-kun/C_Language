106b761437129226f1a47fdeb0b8faccb408f242
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-04-15 14:53:17 +0900

    Create ArrayAddressASCIIFflush.c
    //Check Array name is address name. 
    //Use string like a integer.
     //Fflush Function.

---
+++ DayStudy/ArrayAddressASCIIFflush.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  /*
+  //Check Array name is address name. 
+  int ary[5]={10,20,30,40,50};
+
+  printf("Array name value : %u\n", ary);
+  printf("1st array elements address : %u\n", &ary[0]);
+  printf("Array name elements value : %d\n", *ary);
+  printf("First Array elements value : %d\n", ary[0]);
+  return 0;
+  */
+ /*
+  //Use string like a integer.
+  int ch;    //int type
+
+  printf("'a' in ASCII : %d\n", 'a');
+  printf("ASCII Code at '97' character : %c\n", 97);
+  printf("Character type Number size : %d byte\n", sizeof('a'));
+
+  ch='a';
+  ch++;    //Plus 1 at ASCII Code.
+  printf("Character %c s ASCII Code value : %d\n", ch, ch);
+
+  return 0;
+ */
+ /*
+ //Fflush Function.
+ int num, grade;
+
+ printf("input school number : ");
+ scanf("%d", &num);
+ fflush(stdin);    //Delete for character in buffer.
+ printf("input grade : ");
+ grade = getchar();
+ printf("School number : %d, grade : %c", num, grade);
+
+ return 0;
+ */
+
+}
